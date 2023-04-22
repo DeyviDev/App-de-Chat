19ff09e740f228978ad1a5aeeef7557aa73136c4
Deyvi De la cruz <deyvifcruz@gmail.com>
2023-04-22 10:18:28 -0700

    Add project files.

---
+++ ChatApp/MauiProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace ChatApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Metropolis-Black.otf", "Metropolis Black");
+                fonts.AddFont("Metropolis-Ligh.otf", "Metropolis Ligh");
+                fonts.AddFont("Metropolis-Medium.otf", "Metropolis Medium");
+                fonts.AddFont("Metropolis-Regular.otf", "Metropolis Regular");
+            });
+
+		return builder.Build();
+	}
+}
